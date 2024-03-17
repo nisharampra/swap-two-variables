b995740c5c5f15f7761538dc198320232bfcb273
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-03-17 22:04:33 +0800

    Add files via upload

---
+++ newProjec/src/newProjec/newprojec.java
@@ -0,0 +1,23 @@
+package newProjec;
+
+public class newprojec {
+
+    public newprojec() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+
+        String x = "water";
+        String y = "kool aid";
+        String temp;
+
+        // Swapping logic
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x:" + x);
+        System.out.println("y:" + y);
+    }
+}
